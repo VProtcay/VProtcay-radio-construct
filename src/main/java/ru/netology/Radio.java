ab47dcbdec9bc62ca6a80216db42b4a477f7096e
Vladimir Protcay <protcay2@mail.ru>
2022-03-23 21:38:07 +0300

    first

---
+++ src/main/java/ru/netology/Radio.java
@@ -0,0 +1,82 @@
+package ru.netology;
+
+public class Radio {
+
+    private int currentStation;
+    private int volume;
+    private int numberOfStations = 10;
+
+    public Radio(int numberOfStations) {
+        this.numberOfStations = numberOfStations;
+    }
+
+    public Radio() {
+    }
+
+    public int getNumberOfStations() {
+        return numberOfStations;
+    }
+
+    public void setNumberOfStations(int numberOfStations) {
+        if (numberOfStations < 0) {
+            return;
+        }
+        this.numberOfStations = numberOfStations;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < 0) {
+            return;
+        }
+        if (currentStation > numberOfStations) {
+            return;
+        }
+        this.currentStation = currentStation;
+    }
+
+    public void nextStation() {
+        if (currentStation < numberOfStations) {
+            currentStation = currentStation + 1;
+        } else {
+            currentStation = 0;
+        }
+    }
+
+    public void prevStation() {
+        if (currentStation > 0) {
+            currentStation = currentStation - 1;
+        } else {
+            currentStation = numberOfStations;
+        }
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        if (volume < 0) {
+            return;
+        }
+        if (volume > 100) {
+            return;
+        }
+        this.volume = volume;
+    }
+
+    public void increaseVolume() {
+        if (volume < 100) {
+            volume = volume + 1;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (volume > 0) {
+            volume = volume - 1;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/RadioTest.java
@@ -0,0 +1,136 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RadioTest {
+    Radio radio = new Radio();
+    Radio radio1 = new Radio(20);
+
+    @Test
+    void shouldChekSetCurrentStationIsNumberOfStations() {
+        radio.setCurrentStation(10);
+        assertEquals(10, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldChekSetCurrentStationOverNumbersOfStations() {
+        radio1.setCurrentStation(21);
+        assertEquals(0, radio1.getCurrentStation());
+    }
+
+    @Test
+    void shouldCheckSetCurrentStationLessZero() {
+        radio.setCurrentStation(-1);
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldCheckSetNumberOfStationsLessZero() {
+        radio1.setNumberOfStations(-1);
+        assertEquals(20, radio1.getNumberOfStations());
+    }
+
+    @Test
+    void shouldCheckSetNumberOfStationsAboveZero() {
+        radio1.setNumberOfStations(1);
+        assertEquals(1, radio1.getNumberOfStations());
+    }
+
+    @Test
+    void shouldCheckNextStationWhenCurrentStationIsZero() {
+        radio.setCurrentStation(0);
+        radio.nextStation();
+
+        assertEquals(1, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldCheckNextStationWhenCurrentStationIsNumberOfStations() {
+        radio.setCurrentStation(10);
+        radio.nextStation();
+
+        assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldCheckNextStationWhenCurrentStationAboveNumberOfStations() {
+        radio1.setCurrentStation(21);
+        radio1.nextStation();
+
+        assertEquals(1, radio1.getCurrentStation());
+    }
+
+    @Test
+    void shouldCheckNextStationWhenCurrentStationInTheMiddle() {
+        radio.setCurrentStation(5);
+        radio.nextStation();
+
+        assertEquals(6, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldCheckPrevStationWhenCurrentStationIsZero() {
+        radio.setCurrentStation(0);
+        radio.prevStation();
+
+        assertEquals(10, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldCheckPrevStationWhenCurrentStationIsNumberOfStations() {
+        radio1.setCurrentStation(20);
+        radio1.prevStation();
+
+        assertEquals(19, radio1.getCurrentStation());
+    }
+
+    @Test
+    void shouldCheckPrevStationWhenCurrentStationAboveNumberOfStations() {
+        radio.setCurrentStation(11);
+        radio.prevStation();
+
+        assertEquals(10, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldCheckPrevStationWhenCurrentStationInTheMiddle() {
+        radio.setCurrentStation(6);
+        radio.prevStation();
+
+        assertEquals(5, radio.getCurrentStation());
+    }
+
+    @Test
+    void shouldCheckIncreaseVolumeIfSetCurrentVolumeAboveOneHundred() {
+        radio.setVolume(111);
+        radio.increaseVolume();
+
+        assertEquals(1, radio.getVolume());
+    }
+
+    @Test
+    void shouldCheckIncreaseVolumeIfSetCurrentVolumeIsOneHundred() {
+        radio.setVolume(100);
+        radio.increaseVolume();
+
+        assertEquals(100, radio.getVolume());
+    }
+
+    @Test
+    void shouldCheckDecreaseVolumeIfSetCurrentVolumeAboveZero() {
+        radio.setVolume(100);
+        radio.decreaseVolume();
+
+        assertEquals(99, radio.getVolume());
+    }
+
+    @Test
+    void shouldCheckDecreaseVolumeIfSetCurrentVolumeLessZero() {
+        radio.setVolume(-1);
+        radio.decreaseVolume();
+
+        assertEquals(0, radio.getVolume());
+    }
+    }
